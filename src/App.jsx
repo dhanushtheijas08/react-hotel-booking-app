8e3d68dd4428e1c2b4081a6420ac8110bdedbdd1
dhanushtheijas08 <21cs033@kpriet.ac.in>
2023-10-13 09:06:51 +0530

    added new table

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import Account from "./pages/Account";
 import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./ui/AppLayout";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   const queryClient = new QueryClient();
@@ -35,6 +36,22 @@ function App() {
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
+      <Toaster
+        position="top-right"
+        gutter={12}
+        containerStyle={{ margin: "8px" }}
+        toastOptions={{
+          success: { duration: 1500 },
+          error: { duration: 2000 },
+          style: {
+            fontSize: "16px",
+            maxWidth: "500px",
+            padding: "16px 24px",
+            backgroundColor: "var(--color-grey-0)",
+            color: "var(--color-grey-700)",
+          },
+        }}
+      />
     </QueryClientProvider>
   );
 }
--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -149,6 +149,7 @@ import styled from "styled-components";
 import { formatCurrency } from "../../utils/helpers";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { deleteCabin } from "../../services/apiCabins";
+import toast from "react-hot-toast";
 
 const TableRow = styled.div`
   display: grid;
@@ -196,9 +197,13 @@ export default function CabinRow({ cabin }) {
     mutationKey: ["cabin"],
     mutationFn: (id) => deleteCabin(id),
     onSuccess: () => {
+      toast.success("Successfully toasted!");
       queryClient.invalidateQueries({
         queryKey: ["cabin"],
       });
+    },
+    onError: (err) => {
+      toast.error(err.message);
     },
   });
 
