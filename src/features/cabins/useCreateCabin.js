7d52eb65c02fa1fcbd89a19a02077ccd351ceafe
dhanushtheijas08 <21cs033@kpriet.ac.in>
2023-10-14 16:00:52 +0530

    added custom hook for creating

--- src/features/cabins/useCreateCabin.js
+++ src/features/cabins/useCreateCabin.js
@@ -1,4 +1,4 @@
-import { useQueryClient } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { createCabin } from "../../services/apiCabins";
 import toast from "react-hot-toast";
 
